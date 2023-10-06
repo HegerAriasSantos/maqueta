d54077659035f2061dd70cbe80ad223b4f2abdb4
Heger Arias Santos <hegel.0218@gmail.com>
2023-10-06 06:07:59 -0400

    implemented tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'mequeta';
+  newTodoText = '';
+  todos: Todo[] = [];
+
+  constructor() {}
+
+  addTodo(): void {
+    if (this.newTodoText.trim() !== '') {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: this.newTodoText,
+        completed: false,
+      };
+      this.todos.push(newTodo);
+      this.newTodoText = '';
+    }
+  }
+
+  toggleTodo(todo: Todo): void {
+    todo.completed = !todo.completed;
+  }
+
+  deleteTodo(todo: Todo): void {
+    const index = this.todos.findIndex((t) => t.id === todo.id);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
