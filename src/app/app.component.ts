d0ad715911aeaaab49f51a0edc51bc11eb4d5ff7
Heger Arias Santos <hegel.0218@gmail.com>
2023-10-08 01:40:03 -0400

    si si

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,157 @@
 import { Component, HostBinding } from '@angular/core';
-import { Carousel, Dropdown, initTE, Collapse } from 'tw-elements';
+import { Carousel, Dropdown, initTE, Collapse, Datatable } from 'tw-elements';
 
 export interface Todo {
   id: number;
   text: string;
   completed: boolean;
 }
+const data = {
+  columns: [
+    {
+      label: 'Name',
+      field: 'name',
+    },
+    'Position',
+    'Office',
+    'Age',
+    'Start date',
+    'Salary',
+  ],
+  rows: [
+    [
+      'Tiger Nixon',
+      'System Architect',
+      'Edinburgh',
+      '61',
+      '2011/04/25',
+      '$320,800',
+    ],
+    ['Garrett Winters', 'Accountant', 'Tokyo', '63', '2011/07/25', '$170,750'],
+    [
+      'Ashton Cox',
+      'Junior Technical Author',
+      'San Francisco',
+      '66',
+      '2009/01/12',
+      '$86,000',
+    ],
+    [
+      'Cedric Kelly',
+      'Senior Javascript Developer',
+      'Edinburgh',
+      '22',
+      '2012/03/29',
+      '$433,060',
+    ],
+    ['Airi Satou', 'Accountant', 'Tokyo', '33', '2008/11/28', '$162,700'],
+    [
+      'Brielle Williamson',
+      'Integration Specialist',
+      'New York',
+      '61',
+      '2012/12/02',
+      '$372,000',
+    ],
+    [
+      'Herrod Chandler',
+      'Sales Assistant',
+      'San Francisco',
+      '59',
+      '2012/08/06',
+      '$137,500',
+    ],
+    [
+      'Rhona Davidson',
+      'Integration Specialist',
+      'Tokyo',
+      '55',
+      '2010/10/14',
+      '$327,900',
+    ],
+    [
+      'Colleen Hurst',
+      'Javascript Developer',
+      'San Francisco',
+      '39',
+      '2009/09/15',
+      '$205,500',
+    ],
+    [
+      'Sonya Frost',
+      'Software Engineer',
+      'Edinburgh',
+      '23',
+      '2008/12/13',
+      '$103,600',
+    ],
+    ['Jena Gaines', 'Office Manager', 'London', '30', '2008/12/19', '$90,560'],
+    [
+      'Quinn Flynn',
+      'Support Lead',
+      'Edinburgh',
+      '22',
+      '2013/03/03',
+      '$342,000',
+    ],
+    [
+      'Charde Marshall',
+      'Regional Director',
+      'San Francisco',
+      '36',
+      '2008/10/16',
+      '$470,600',
+    ],
+    [
+      'Haley Kennedy',
+      'Senior Marketing Designer',
+      'London',
+      '43',
+      '2012/12/18',
+      '$313,500',
+    ],
+  ],
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  newTodoText = '';
-  todos: Todo[] = [];
-  darkMode = true;
-  @HostBinding('class.dark') get mode() {
-    return this.darkMode;
-  }
-
   constructor() {}
 
   ngOnInit() {
     initTE({ Carousel, Dropdown, Collapse });
-  }
 
-  addTodo(): void {
-    if (this.newTodoText.trim() !== '') {
-      const newTodo: Todo = {
-        id: Date.now(),
-        text: this.newTodoText,
-        completed: false,
-      };
-      this.todos.push(newTodo);
-      this.newTodoText = '';
-    }
-  }
+    const instance = new Datatable(document.getElementById('datatable'), data);
+    const advancedSearchInput = document.getElementById(
+      'advanced-search-input'
+    )! as any;
 
-  toggleTodo(todo: Todo): void {
-    todo.completed = !todo.completed;
-  }
+    const search = (value: string) => {
+      let [phrase, columns] = value
+        .split(' in:')
+        .map((str) => str.trim()) as any;
 
-  deleteTodo(todo: Todo): void {
-    const index = this.todos.findIndex((t) => t.id === todo.id);
-    if (index !== -1) {
-      this.todos.splice(index, 1);
-    }
+      if (columns) {
+        columns = columns
+          .split(',')
+          .map((str: string) => str.toLowerCase().trim());
+      }
+
+      instance.search(phrase, columns);
+    };
+
+    document
+      .getElementById('advanced-search-button')!
+      .addEventListener('click', (e) => {
+        search(advancedSearchInput.value);
+      });
+
+    advancedSearchInput.addEventListener('keydown', (e: any) => {
+      if (e.keyCode === 13) {
+        search(e.target.value);
+      }
+    });
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,7 @@ module.exports = {
     "./src/**/*.{html,ts}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
-  theme: {
-    colors: {
-      darkBG: "#1e2227",
-      darkHover: "#2c313a",
-      lightBG: "#f8f8f8",
-      lightHover: "#f2f2f2",
-      header:"#282c34"
-    },
-  },
+ 
   darkMode: "class",
   plugins: [require("tw-elements/dist/plugin.cjs")],
 };
